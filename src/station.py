004562a93e497d5a151252cd29ebd97d4041a49b
Gergely Imreh <imrehg@gmail.com>
2016-06-27 17:58:43 +0800

    tune server code

--- src/station.py
+++ src/station.py
@@ -7,9 +7,14 @@ import Adafruit_BMP.BMP085 as BMP085   # Actually using it for BMP180
 import Adafruit_BBIO.GPIO as GPIO
 
 def blink(pin, blinktime=0.1):
-    GPIO.output(pin, GPIO.HIGH)
+    blinks([pins], blinktime)
+
+def blinks(pins, blinktime=0.1):
+    for pin in pins:
+        GPIO.output(pin, GPIO.HIGH)
     time.sleep(blinktime)
-    GPIO.output(pin, GPIO.LOW)
+    for pin in pins:
+        GPIO.output(pin, GPIO.LOW)
 
 if __name__ == "__main__":
     # Set up GPIO pins
@@ -21,8 +26,8 @@ if __name__ == "__main__":
     GPIO.setup(pin1, GPIO.OUT)
     GPIO.output(pin1, GPIO.LOW)
 
-    blinkshort = 0.1
-    blinklong = 0.6
+    blinkshort = 0.05
+    blinklong = 0.8
 
     sensor = BMP085.BMP085(busnum=2, mode=BMP085.BMP085_ULTRAHIGHRES)
 
@@ -52,6 +57,14 @@ if __name__ == "__main__":
     if PERIOD < 1:
         PERIOD = 1
 
+    try:
+        # different display trashhold, in units of X unit/min, above which do long blink
+        SENSOR_THRESHOLD = float(os.getenv('PERIOD', default='1.0'))
+    except ValueError:
+        SENSOR_THRESHOLD = 1.0
+    if SENSOR_THRESHOLD < 0:
+        SENSOR_THRESHOLD = 1.0
+
     while True:
         time.sleep(PERIOD - blinktime)
         x = reading()
@@ -60,10 +73,10 @@ if __name__ == "__main__":
         b = beta * (a - aold) + (1 - beta) * bold
         print("{0:0.1f},{1:0.3f},{2:0.3f}".format(x, a, b))
         # Do long blink if temperature change is more than 1 unit/min
-        blinktime = blinklong if abs(b) >= 1 / 60.0 * PERIOD else blinkshort
-        if b < 0:
-            # print("Blink: {}".format(pin0))
+        blinktime = blinklong if abs(b) >= SENSOR_THRESHOLD / 60.0 * PERIOD else blinkshort
+        if abs(b) < 0.001:
+            blinks([pin0, pin1], blinktime)
+        elif b < 0:
             blink(pin0, blinktime)
         else:
-            # print("Blink: {}".format(pin1))
             blink(pin1, blinktime)
